37d157e0f1eff54c53fbb10fb1b0650c55b5cd75
MesutAlverü <mesutalver@icloud.com>
2022-11-05 15:04:00 +0300

    Initial Commit

---
+++ tableViewDraft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewDraft
+//
+//  Created by mesut alver on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
